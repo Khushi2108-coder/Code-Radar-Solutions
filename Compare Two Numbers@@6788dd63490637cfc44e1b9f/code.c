ff918fae6b488b0243b42ecc1eb1b164cc654ba1
Khushi2108-coder <khushi1194.becse24@chitkara.edu.in>
2025-02-04 23:53:24 +0530

    Solution to Compare Two Numbers

---
+++ Compare Two Numbers@@6788dd63490637cfc44e1b9f/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int x,y;
+    scanf("%d",&x,&y);
+    if(x>y){
+        printf("First");
+    }
+    else if{
+        printf("Second");
+    }
+    else{
+        printf("Equal");
+    }
+    return 0;
+}
